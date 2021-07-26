6813e22f1ef955c9741b176c24b674a3b266f8f9
bo-iliev <boiliev44@gmail.com>
2021-07-26 10:27:30 +0300

    added background and fixed logic

--- src/App.js
+++ src/App.js
@@ -1,47 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import useMovement from './useMovement';
 
 export default function App() {
   const canvasRef = useRef(null);
-
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const linkDownRef = useRef(null);
+  const linkUpRef = useRef(null);
+  const linkLeftRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const { x, y, direction, move } = useMovement();
 
   // Set the height and width of canvas
   useEffect(() => {
     const context = canvasRef.current.getContext('2d');
     context.canvas.height = window.innerHeight;
     context.canvas.width = window.innerWidth;
-
-    context.fillRect(x, y, 100, 100);
   }, []);
 
   //Move the box if x or y changes
   useEffect(() => {
     const context = canvasRef.current.getContext('2d');
     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    context.fillRect(x, y, 100, 100);
+    // context.fillRect(x, y, 100, 100);
+    let theLinkRef;
+    if (direction === 'down') theLinkRef = linkDownRef;
+    if (direction === 'up') theLinkRef = linkUpRef;
+    if (direction === 'left') theLinkRef = linkLeftRef;
+    if (direction === 'right') theLinkRef = linkRightRef;
+
+    context.drawImage(theLinkRef.current, x, y);
   }, [x, y]);
-  // Add event listener to window to listen for arrow keys
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-
-    function handleKeyDown(e) {
-      if (e.key === 'ArrowUp') move('up');
-      if (e.key === 'ArrowDown') move('down');
-      if (e.key === 'ArrowLeft') move('left');
-      if (e.key === 'ArrowRight') move('right');
-    }
-
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, []);
-
-  function move(direction) {
-    if (direction === 'up') setY((y) => y - 20);
-    if (direction === 'down') setY((y) => y + 20);
-    if (direction === 'left') setX((x) => x - 20);
-    if (direction === 'right') setX((x) => x + 20);
-  }
 
   return (
     <div className="app">
@@ -55,10 +43,22 @@ export default function App() {
       </div>
 
       <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+        <img
+          ref={linkDownRef}
+          src="https://i.imgur.com/JYUB0m3.png"
+          alt="Down"
+        />
+        <img
+          ref={linkRightRef}
+          src="https://i.imgur.com/GEXD7bk.gif"
+          alt="Right"
+        />
+        <img ref={linkUpRef} src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img
+          ref={linkLeftRef}
+          src="https://i.imgur.com/4LGAZ8t.gif"
+          alt="Left"
+        />
       </div>
     </div>
   );
